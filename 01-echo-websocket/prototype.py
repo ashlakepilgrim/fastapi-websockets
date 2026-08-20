3bd81164b80b300b0872399f1be57162a18e56ce
ashlakepilgrim <206718795+ashlakepilgrim@users.noreply.github.com>
2026-08-21 01:50:02 +0900

    chore(echo-websocket): commit prototype for future references

---
+++ 01-echo-websocket/prototype.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<html>
+    <head>
+        <title>Echo WebSocket</title>
+    </head>
+    <body>
+        <h1>Echo WebSocket</h1>
+        <form onsubmit="sendMessage(event)">
+            <input id="input-textbox" type="text" />
+            <button>Send</button>
+        </form>
+        <hr>
+        <h2>Logs</h2>
+        <ul id="messages"></ul>
+        <script>
+            const ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages');
+                var message = document.createElement('li');
+                var content = document.createTextNode("SERVER: " + event.data);
+                message.appendChild(content);
+                messages.appendChild(message);
+            }
+            function sendMessage(event) {
+                event.preventDefault();
+                var input = document.getElementById('input-textbox');
+                var messages = document.getElementById('messages');
+                var message = document.createElement('li');
+                var content = document.createTextNode("CLIENT: " + input.value);
+                message.appendChild(content);
+                messages.appendChild(message);
+                ws.send(input.value);
+                input.value = '';
+            }
+        </script>
+    </body>
+</html>
+"""
+
+@app.get("/", response_class=HTMLResponse)
+async def get_root():
+    return html
+
+@app.websocket("/ws")                                               # define the websocket endpoint
+async def websocket_endpoint(websocket: WebSocket):                 # receive incoming websocket connection
+    await websocket.accept()                                        # accept the ws connection
+    while True:                                                     # start a loop
+        message = await websocket.receive_text()                    # wait until the client sends me a text message
+        await websocket.send_text(message)                          # send the message as soon as it is available
+
+# javascript
+# =======================
+# const ws = new WebSocket("ws://localhost:8000/ws");
+# ws;
+# ws.onmessage = (event) => {console.log("SERVER: ", event.data)}
+# ws.send("hello")
